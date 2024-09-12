2b4d44971e0fa4d9975210fa68618ff49bc5a051
MadushaniR <madushani.2019360@iit.ac.lk>
2024-09-12 23:36:03 +0530

    draggble option added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,100 @@
-import React from 'react';
-import KanbanBoard from '../KanbanBoard/KanbanBoard';
+import './home.css'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import mockData from './mockData'
+import { useState } from 'react'
+import Card from '../../Components/Card/Card'
+
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Kanban Board</h1>
-      <KanbanBoard />
-    </div>
-  );
-};
+    const [data, setData] = useState(mockData)
 
-export default Home;
+    const onDragEnd = (result) => {
+        if (!result.destination) return;
+        const { source, destination } = result;
+    
+        if (source.droppableId !== destination.droppableId) {
+            const sourceColIndex = data.findIndex(e => e.id === source.droppableId);
+            const destinationColIndex = data.findIndex(e => e.id === destination.droppableId);
+    
+            const sourceCol = data[sourceColIndex];
+            const destinationCol = data[destinationColIndex];
+    
+            const sourceTask = [...sourceCol.tasks];
+            const destinationTask = [...destinationCol.tasks];
+    
+            const [removed] = sourceTask.splice(source.index, 1);
+            destinationTask.splice(destination.index, 0, removed);
+    
+            const newData = [...data];  
+            newData[sourceColIndex] = {
+                ...sourceCol,
+                tasks: sourceTask
+            };
+            newData[destinationColIndex] = {
+                ...destinationCol,
+                tasks: destinationTask
+            };
+    
+            setData(newData);  
+        }
+    };
+    
+    
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <div className="kanban">
+                {
+                    data.map(section => (
+                        <Droppable
+                            key={section.id}
+                            droppableId={section.id}
+                        >
+                            {(provided) => (
+                                <div
+                                    {...provided.droppableProps}
+                                    className='kanban__section'
+                                    ref={provided.innerRef}
+                                >
+                                    <div className="kanban__section__title">
+                                        {section.title}
+                                    </div>
+                                    <div className="kanban__section__content">
+                                        {
+                                            section.tasks.map((task, index) => (
+                                                <Draggable
+                                                    key={task.id}
+                                                    draggableId={task.id}
+                                                    index={index}
+                                                >
+                                                    {(provided, snapshot) => (
+                                                        <div
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            style={{
+                                                                ...provided.draggableProps.style,
+                                                                opacity: snapshot.isDragging ? '0.5' : '1'
+                                                            }}
+                                                        >
+                                                            <Card>
+                                                                {task.title}
+                                                            </Card>
+                                                        </div>
+                                                    )}
+                                                </Draggable>
+                                            ))
+                                        }
+                                        {provided.placeholder}
+                                    </div>
+                                </div>
+                            )}
+                        </Droppable>
+                    ))
+                }
+            </div>
+        </DragDropContext>
+    )
+}
+
+export default Home
